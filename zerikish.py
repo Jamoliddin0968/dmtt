5f93073f8c519feb260539b9aa03f8c5a50cd75b
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-23 22:01:28 +0500

    sadfg

--- zerikish.py
+++ zerikish.py
@@ -1,28 +1,77 @@
-from xhtml2pdf import pisa
+from docx2pdf import convert
+from docx import Document
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Pt
 
-# Function to convert HTML file to PDF
+# Yangi Word hujjat yaratish
+doc = Document()
+
+# ------------------------------------------------------
+paragraph = doc.add_paragraph()
+text = "06.09.2023 sanadagi 128-sonli shartnomaga\n 01.09-27.09 2023 sanalardagi 03/09-sonli\n"
+font = paragraph.add_run(text).font
+font.name = 'Times New Roman'
+font.size = Pt(11)
+paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+# ------------------------------------------
+paragraph_text = "HISOBVARAQ-FAKTURA"
+paragraph = doc.add_paragraph()
+run = paragraph.add_run(paragraph_text)
+
+font = run.font
+font.name = 'Times New Roman'
+font.size = Pt(19)
+paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+# -----------------------------------------------------------
 
 
-def convert_html_file_to_pdf(input_html_file, output_pdf_file):
-    # Read HTML content from file
-    with open(input_html_file, 'r', encoding='utf-8') as file:
-        html_content = file.read()
+data = [
+    [
+        {"Key": "Yetkazib beruvchi:", "Value": "Muhammadqodir"},
+        {"Key": "Xaridor:", "Value": "df"},
+    ],
+    [
+        {"Key": "Manzil:", "Value": "Toshloq"},
+        {"Key": "Manzil:", "Value": "None"},
+    ],
+    [
+        {"Key": "Tel:", "Value": "+9983698008"},
+        {"Key": "Tel:", "Value": "none"},
+    ],
+    [
+        {"Key": "STIR:", "Value": "7895152"},
+        {"Key": "STIR:", "Value": "None"}
+    ]
+]
 
-    # Create a PDF file
-    with open(output_pdf_file, 'wb') as output_pdf:
-        # Convert HTML to PDF
-        pisa_status = pisa.CreatePDF(html_content, dest=output_pdf)
-
-    if pisa_status.err:
-        print(f"Failed to generate PDF: {pisa_status.err}")
-    else:
-        print(f"PDF successfully generated: {output_pdf_file}")
+# Add a table with 4 columns and 4 rows without lines and borders
+columns = 4
+rows = 4
+table = doc.add_table(rows=rows, cols=columns)
+table.style = 'Table Grid'
 
 
-# Input HTML file path and output PDF file path
-# Replace with the path to your HTML file
-input_html_path = 'D:/Projects/Html/doc/index.html'
-output_pdf_path = 'output.pdf'  # Replace with the desired output PDF file path
+for i in range(rows):
+    for j in range(columns):
+        cell = table.cell(i, j)
+        key = data[i][j//2]["Key"]
+        value = data[i][j//2]["Value"]
+        # Set styles for keys (column 1) and values (column 2)
+        if j % 2 == 0:  # If the cell is in the left column
+            key_run = cell.paragraphs[0].add_run(key)
+            key_run.font.name = 'Times New Roman'
+            key_run.font.size = Pt(11)
+            key_run.font.bold = True
+            cell.paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        else:  # If the cell is in the right column
+            value_run = cell.paragraphs[0].add_run(
+                value if value else "")  # Add value or empty string if None
+            value_run.font.name = 'Times New Roman'
+            value_run.font.size = Pt(11)
 
-# Convert HTML file to PDF
-convert_html_file_to_pdf(input_html_path, output_pdf_path)
+
+# ---------------------------------------------------------
+
+
+doc.save('mavzu.docx')
+convert("mavzu.docx", "output.pdf")
